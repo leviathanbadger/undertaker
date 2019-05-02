15039cf11907a4d8c7d98e4d6448c2bc53f590e3
Brandon Slade <brandonyoyoslade@gmail.com>
2019-05-01 20:41:04 -0500

    refactor(*): remove 'Implementation' namespace

--- Undertaker/Implementation/DefaultActivator.cs
+++ Undertaker/Implementation/DefaultActivator.cs
@@ -2,7 +2,7 @@
 using System.Reflection;
 using System.Threading;
 
-namespace Undertaker.Implementation
+namespace Undertaker
 {
     public class DefaultActivator : IActivator
     {
--- Undertaker/Implementation/JobBuilder.cs
+++ Undertaker/Implementation/JobBuilder.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
 
-namespace Undertaker.Implementation
+namespace Undertaker
 {
     public class JobBuilder : IJobBuilder
     {
