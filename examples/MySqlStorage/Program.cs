5159b580a90603074268e2cc79ae1def833a1866
Brandon Slade <brandonyoyoslade@gmail.com>
2019-05-31 22:54:30 -0500

    chore(*): add examples folder with empty MySqlStorage example project

---
+++ examples/MySqlStorage/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MySqlStorage
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            new Program().Run();
+        }
+
+        private void Run()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
