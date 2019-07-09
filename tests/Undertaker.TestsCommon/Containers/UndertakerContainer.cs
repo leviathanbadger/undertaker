a47f66aede087001c65a3ddd00be02c64f6fbf08
Brandon Slade <brandonyoyoslade@gmail.com>
2019-07-08 20:47:39 -0500

    test(common): add UndertakerContainer with AutoFixtureRegistry

---
+++ tests/Undertaker.TestsCommon/Containers/UndertakerContainer.cs
@@ -0,0 +1,38 @@
+﻿using AutoFixture;
+using AutoFixture.Dsl;
+using Lamar;
+using System;
+using Undertaker.TestsCommon.Registries;
+
+namespace Undertaker.TestsCommon.Containers
+{
+    public class UndertakerContainer
+    {
+        private readonly IContainer _container;
+
+        public UndertakerContainer(Action<ServiceRegistry> afterRegisterFn = null)
+        {
+            _container = new Container(services =>
+            {
+                services.IncludeRegistry<AutoFixtureRegistry>();
+
+                afterRegisterFn?.Invoke(services);
+            });
+        }
+
+        public T Get<T>()
+        {
+            return _container.GetInstance<T>();
+        }
+
+        public T Fake<T>()
+        {
+            return Get<Fixture>().Create<T>();
+        }
+
+        public ICustomizationComposer<T> FakeBuild<T>()
+        {
+            return Get<Fixture>().Build<T>();
+        }
+    }
+}
---
+++ tests/Undertaker.TestsCommon/Registries/AutoFixtureRegistry.cs
@@ -0,0 +1,20 @@
+﻿using AutoFixture;
+using Lamar;
+
+namespace Undertaker.TestsCommon.Registries
+{
+    public class AutoFixtureRegistry : ServiceRegistry
+    {
+        public AutoFixtureRegistry()
+        {
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            this.For<Fixture>()
+                .Use(ctx => new Fixture())
+                .Singleton();
+        }
+    }
+}
