a6c55400ea2429c99baffb2ac1946fd584f48952
Brandon Slade <brandonyoyoslade@gmail.com>
2019-07-08 20:53:32 -0500

    test(core): add simple unit test for InMemoryJobStorage

---
+++ tests/Undertaker.Tests/Implementation/InMemoryJobStorageFacts.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using System;
+using Undertaker.TestsCommon.Containers;
+using Xunit;
+
+namespace Undertaker
+{
+    public class InMemoryJobStorageFacts
+    {
+        private readonly UndertakerContainer _container = new UndertakerContainer();
+
+        [Fact]
+        public void CreateJob_ShouldWork()
+        {
+            //Arrange
+            var jobName = "BillyBobJoe";
+            var jobDesc = "Description goes here.";
+            var parameters = Array.Empty<ParameterDefinition>();
+            var runAt = DateTime.UtcNow.AddMinutes(5);
+            var runAfter = Array.Empty<IJob>();
+
+            var jobDefinition = new JobDefinition(jobName, jobDesc, "", "", false, parameters, runAt, runAfter);
+
+            var jobStorage = _container.Get<InMemoryJobStorage>();
+
+            //Act
+            var job = jobStorage.CreateJob(jobDefinition);
+
+            //Assert
+            job.Should().NotBeNull();
+            job.Storage.Should().Be(jobStorage);
+            job.Name.Should().Be(jobName);
+            job.Description.Should().Be(jobDesc);
+        }
+    }
+}
