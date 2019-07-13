4c1b1bfaada327a85823bf5de4790cf756871a24
Brandon Slade <brandonyoyoslade@gmail.com>
2019-07-13 16:12:31 -0500

    feat(core): add worker class

---
+++ src/Undertaker/Agent/Worker.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Undertaker.Agent
+{
+    internal class Worker
+    {
+        const int MillisBetweenStopCheck = 200;
+
+        private readonly object _syncLock = new object();
+
+        private bool _isRunning;
+        private Thread _workerThread;
+
+        private readonly IJobStorage _storage;
+        private readonly IActivator _activator;
+
+        public Worker(
+            IJobStorage storage,
+            IActivator activator)
+        {
+            if (storage == null) throw new ArgumentNullException(nameof(storage));
+            if (activator == null) throw new ArgumentNullException(nameof(activator));
+            _storage = storage;
+            _activator = activator;
+        }
+
+        public void Start()
+        {
+            lock (_syncLock)
+            {
+                if (_isRunning) throw new InvalidOperationException("This worker is already running!");
+                if (_workerThread != null) throw new InvalidOperationException("This worker has already been started and stopped once. Please create a new worker.");
+                _isRunning = true;
+                _workerThread = new Thread(() => WorkerLoop().Wait());
+                _workerThread.Start();
+            }
+        }
+        public void Stop(bool waitForWorkers)
+        {
+            Thread workerThread;
+            lock (_syncLock)
+            {
+                _isRunning = false;
+                workerThread = _workerThread;
+            }
+
+            if (waitForWorkers) workerThread?.Join();
+        }
+
+        private async Task WorkerLoop()
+        {
+            while (true)
+            {
+                if (ShouldStopWorkerLoop()) return;
+
+                var nextJob = await PollForNextJobAsync();
+                if (nextJob != null)
+                {
+                    await ProcessJobAsync(nextJob);
+                }
+                else
+                {
+                    for (int q = 0; q < 5000 / MillisBetweenStopCheck; q++)
+                    {
+                        await Task.Delay(MillisBetweenStopCheck);
+                        if (ShouldStopWorkerLoop()) return;
+                    }
+                }
+            }
+        }
+
+        private bool ShouldStopWorkerLoop()
+        {
+            lock (_syncLock)
+            {
+                return !_isRunning;
+            }
+        }
+
+        private async Task<IJob> PollForNextJobAsync()
+        {
+            return await _storage.PollForNextJobAsync();
+        }
+
+        private Task ProcessJobAsync(IJob job)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ tests/Undertaker.Tests/Agent/WorkerFacts.cs
@@ -0,0 +1,109 @@
+﻿using FluentAssertions;
+using NSubstitute;
+using System;
+using System.Threading.Tasks;
+using Undertaker.TestsCommon.Containers;
+using Xunit;
+
+namespace Undertaker.Agent
+{
+    public class WorkerFacts
+    {
+        private readonly UndertakerContainer _container = new UndertakerContainer();
+
+        [Fact]
+        public void Ctor_WhenStorageIsNull_ShouldFailFast()
+        {
+            //Arrange
+            var activator = _container.Get<IActivator>();
+
+            //Act
+            Func<object> act = () => new Worker(null, activator);
+
+            //Assert
+            act.Should()
+               .Throw<ArgumentNullException>()
+               .Which.ParamName.Should().Be("storage");
+        }
+
+        [Fact]
+        public void Ctor_WhenActivatorIsNull_ShouldFailFast()
+        {
+            //Arrange
+            var storage = _container.Get<IJobStorage>();
+
+            //Act
+            Func<object> act = () => new Worker(storage, null);
+
+            //Assert
+            act.Should()
+               .Throw<ArgumentNullException>()
+               .Which.ParamName.Should().Be("activator");
+        }
+
+        [Fact]
+        public async Task Start_ShouldBeginPollingForJobs()
+        {
+            //Arrange
+            var storage = _container.Get<IJobStorage>();
+            var activator = _container.Get<IActivator>();
+            storage.PollForNextJobAsync().Returns(Task.FromResult<IJob>(null));
+            var worker = new Worker(storage, activator);
+
+            //Act
+            worker.Start();
+            await Task.Delay(200);
+            worker.Stop(true);
+
+            //Assert
+            await storage.Received().PollForNextJobAsync();
+        }
+
+        [Fact]
+        public void Start_WhenWorkerIsAlreadyRunning_ShouldFailFast()
+        {
+            //Arrange
+            var storage = _container.Get<IJobStorage>();
+            var activator = _container.Get<IActivator>();
+            storage.PollForNextJobAsync().Returns(Task.FromResult<IJob>(null));
+            var worker = new Worker(storage, activator);
+
+            try
+            {
+                //Act
+                worker.Start();
+                Action act = () => worker.Start();
+
+                //Assert
+                act.Should()
+                   .Throw<InvalidOperationException>()
+                   .WithMessage("*already running*");
+            }
+            finally
+            {
+                //Cleanup
+                worker.Stop(true);
+            }
+        }
+
+        [Fact]
+        public void Start_WhenWorkerWasStopped_ShouldFailFast()
+        {
+            //Arrange
+            var storage = _container.Get<IJobStorage>();
+            var activator = _container.Get<IActivator>();
+            storage.PollForNextJobAsync().Returns(Task.FromResult<IJob>(null));
+            var worker = new Worker(storage, activator);
+
+            //Act
+            worker.Start();
+            worker.Stop(true);
+            Action act = () => worker.Start();
+
+            //Assert
+            act.Should()
+               .Throw<InvalidOperationException>()
+               .WithMessage("*already* started and stopped once* create a new worker*");
+        }
+    }
+}
