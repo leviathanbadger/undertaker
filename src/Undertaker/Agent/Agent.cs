6f828377f2f4a71a63736308018335155c3ba9a5
Brandon Slade <brandonyoyoslade@gmail.com>
2019-07-13 17:40:55 -0500

    feat(core): add Agent

---
+++ src/Undertaker/Agent/Agent.cs
@@ -0,0 +1,148 @@
+﻿using JetBrains.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Undertaker.Agent
+{
+    public class Agent : IDisposable
+    {
+        private readonly object _syncLock = new object();
+
+        private volatile bool _isDisposed;
+
+        private bool _isRunning;
+        private List<Worker> _workers;
+
+        private IJobStorage _storage;
+        private bool _ownsStorage;
+        private IActivator _activator;
+        private int? _concurrentWorkers;
+
+        public Agent UseStorage([NotNull] IJobStorage storage, bool ownsStorage = true)
+        {
+            if (storage == null) throw new ArgumentNullException(nameof(storage));
+            if (_isDisposed) throw new ObjectDisposedException(nameof(Agent));
+
+            lock (_syncLock)
+            {
+                if (_storage != null) throw new InvalidOperationException("A storage has already been specified for this agent.");
+                _storage = storage;
+                _ownsStorage = ownsStorage;
+            }
+
+            return this;
+        }
+        public Agent UseActivator([NotNull] IActivator activator)
+        {
+            if (activator == null) throw new ArgumentNullException(nameof(activator));
+            if (_isDisposed) throw new ObjectDisposedException(nameof(Agent));
+
+            lock (_syncLock)
+            {
+                if (_activator != null) throw new InvalidOperationException("An activator has already been specified for this agent.");
+                _activator = activator;
+            }
+
+            return this;
+        }
+        public Agent UseConcurrentWorkers(int count)
+        {
+            if (count <= 0) throw new ArgumentException("You must specify at least one concurrent worker.");
+            if (count > 30) throw new ArgumentException("You cannot have more than thirty concurrent workers.");
+            if (_isDisposed) throw new ObjectDisposedException(nameof(Agent));
+
+            lock (_syncLock)
+            {
+                if (_isRunning) throw new NotSupportedException("The number of concurrent workers cannot be changed while the agent is running.");
+                if (_concurrentWorkers != null) throw new InvalidOperationException("The concurrent worker count has already been specified for this agent.");
+                _concurrentWorkers = count;
+            }
+
+            return this;
+        }
+
+        public void Start()
+        {
+            if (_isDisposed) throw new ObjectDisposedException(nameof(Agent));
+            lock (_syncLock)
+            {
+                if (_isRunning) throw new InvalidOperationException("This agent is already running");
+                StartImpl();
+            }
+        }
+        public void Stop(bool waitForWorkers = false)
+        {
+            if (_isDisposed) throw new ObjectDisposedException(nameof(Agent));
+            lock (_syncLock)
+            {
+                StopImpl(waitForWorkers);
+            }
+        }
+
+        private void StartImpl()
+        {
+            if (_storage == null) throw new InvalidOperationException("You must specify a storage using UseStorage before you start the Agent.");
+            if (_activator == null) throw new InvalidOperationException("You must specify an activator using UseActivator before you start the Agent.");
+            if (_workers != null) StopImpl(true); //Make sure all previous workers are completely cleaned up before starting agent again
+            _isRunning = true;
+            _workers = new List<Worker>();
+            var concurrentWorkerCount = _concurrentWorkers ?? 5;
+            for (int q = 0; q < concurrentWorkerCount; q++)
+            {
+                CreateWorker();
+            }
+        }
+        private void CreateWorker()
+        {
+            var worker = new Worker(_storage, _activator);
+            _workers.Add(worker);
+            worker.Start();
+        }
+
+        private void StopImpl(bool waitForWorkers)
+        {
+            _isRunning = false;
+
+            //Soft-stop all workers, so that all can begin shutdown process
+            foreach (var worker in _workers ?? Enumerable.Empty<Worker>())
+            {
+                worker.Stop(false);
+            }
+
+            if (waitForWorkers)
+            {
+                //Hard-stop all workers, so processing doesn't continue until they are cleaned up
+                foreach (var worker in _workers ?? Enumerable.Empty<Worker>())
+                {
+                    worker.Stop(true);
+                }
+                _workers = null; //Proof all workers are cleaned up
+            }
+        }
+
+        public bool IsRunning
+        {
+            get
+            {
+                lock (_syncLock)
+                {
+                    return _isRunning;
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            if (_isDisposed) return;
+            _isDisposed = true;
+
+            lock (_syncLock)
+            {
+                StopImpl(true);
+            }
+
+            if (_ownsStorage) _storage?.Dispose();
+        }
+    }
+}
---
+++ tests/Undertaker.Tests/Agent/AgentFacts.cs
@@ -0,0 +1,328 @@
+﻿using FluentAssertions;
+using NSubstitute;
+using System;
+using System.Threading.Tasks;
+using Undertaker.TestsCommon.Containers;
+using Xunit;
+
+namespace Undertaker.Agent
+{
+    public class AgentFacts
+    {
+        private readonly UndertakerContainer _container = new UndertakerContainer();
+
+        [Fact]
+        public void UseStorage_WhenStorageIsNull_ShouldFailFast()
+        {
+            //Arrange
+            var agent = new Agent();
+            IJobStorage storage = (dynamic)null;
+
+            //Act
+            Action act = () => agent.UseStorage(storage);
+
+            //Assert
+            act.Should()
+               .Throw<ArgumentNullException>()
+               .Which.ParamName.Should().Be(nameof(storage));
+        }
+
+        [Fact]
+        public void UseStorage_WhenAgentIsDisposed_ShouldFailFast()
+        {
+            //Arrange
+            var agent = new Agent();
+            IJobStorage storage = _container.Get<IJobStorage>();
+            agent.Dispose();
+
+            //Act
+            Action act = () => agent.UseStorage(storage);
+
+            //Assert
+            act.Should()
+               .Throw<ObjectDisposedException>()
+               .Which.ObjectName.Should().Be(nameof(Agent));
+        }
+
+        [Fact]
+        public void UseStorage_WhenStorageWasAlreadySpecified_ShouldFailFast()
+        {
+            //Arrange
+            var agent = new Agent();
+            IJobStorage storage = _container.Get<IJobStorage>();
+
+            //Act
+            agent.UseStorage(storage);
+            Action act = () => agent.UseStorage(storage);
+
+            //Assert
+            act.Should()
+               .Throw<InvalidOperationException>()
+               .WithMessage("*storage* already* specified*");
+        }
+
+        [Fact]
+        public void UseActivator_WhenStorageIsNull_ShouldFailFast()
+        {
+            //Arrange
+            var agent = new Agent();
+            IActivator activator = (dynamic)null;
+
+            //Act
+            Action act = () => agent.UseActivator(activator);
+
+            //Assert
+            act.Should()
+               .Throw<ArgumentNullException>()
+               .Which.ParamName.Should().Be(nameof(activator));
+        }
+
+        [Fact]
+        public void UseActivator_WhenAgentIsDisposed_ShouldFailFast()
+        {
+            //Arrange
+            var agent = new Agent();
+            IActivator activator = _container.Get<IActivator>();
+            agent.Dispose();
+
+            //Act
+            Action act = () => agent.UseActivator(activator);
+
+            //Assert
+            act.Should()
+               .Throw<ObjectDisposedException>()
+               .Which.ObjectName.Should().Be(nameof(Agent));
+        }
+
+        [Fact]
+        public void UseActivator_WhenStorageWasAlreadySpecified_ShouldFailFast()
+        {
+            //Arrange
+            var agent = new Agent();
+            IActivator activator = _container.Get<IActivator>();
+
+            //Act
+            agent.UseActivator(activator);
+            Action act = () => agent.UseActivator(activator);
+
+            //Assert
+            act.Should()
+               .Throw<InvalidOperationException>()
+               .WithMessage("*activator* already* specified*");
+        }
+
+        [Fact]
+        public void UseConcurrentWorkers_WhenCountIsZero_ShouldFailFast()
+        {
+            //Arrange
+            var agent = new Agent();
+
+            //Act
+            Action act = () => agent.UseConcurrentWorkers(0);
+
+            //Assert
+            act.Should()
+               .Throw<ArgumentException>()
+               .WithMessage("*at least one concurrent worker*");
+        }
+
+        [Fact]
+        public void UseConcurrentWorkers_WhenCountIsGreaterThanThirty_ShouldFailFast()
+        {
+            //Arrange
+            var agent = new Agent();
+
+            //Act
+            Action act = () => agent.UseConcurrentWorkers(31);
+
+            //Assert
+            act.Should()
+               .Throw<ArgumentException>()
+               .WithMessage("*cannot have more than thirty concurrent workers*");
+        }
+
+        [Fact]
+        public void UseConcurrentWorkers_WhenAgentIsDisposed_ShouldFailFast()
+        {
+            //Arrange
+            var agent = new Agent();
+            agent.Dispose();
+
+            //Act
+            Action act = () => agent.UseConcurrentWorkers(5);
+
+            //Assert
+            act.Should()
+               .Throw<ObjectDisposedException>()
+               .Which.ObjectName.Should().Be(nameof(Agent));
+        }
+
+        [Fact]
+        public void UseConcurrentWorkers_WhenStorageWasAlreadySpecified_ShouldFailFast()
+        {
+            //Arrange
+            var agent = new Agent();
+
+            //Act
+            agent.UseConcurrentWorkers(5);
+            Action act = () => agent.UseConcurrentWorkers(5);
+
+            //Assert
+            act.Should()
+               .Throw<InvalidOperationException>()
+               .WithMessage("*concurrent worker count* already* specified*");
+        }
+
+        [Fact]
+        public void Start_WhenAgentIsDisposed_ShouldFailFast()
+        {
+            //Arrange
+            var agent = new Agent();
+            agent.UseStorage(_container.Get<IJobStorage>())
+                 .UseActivator(_container.Get<IActivator>())
+                 .Dispose();
+
+            //Act
+            Action act = () => agent.Start();
+
+            //Assert
+            act.Should()
+               .Throw<ObjectDisposedException>()
+               .Which.ObjectName.Should().Be(nameof(Agent));
+        }
+
+        [Fact]
+        public void Start_WhenStorageIsNull_ShouldFailFast()
+        {
+            //Arrange
+            var agent = new Agent();
+            agent.UseActivator(_container.Get<IActivator>());
+
+            //Act
+            Action act = () => agent.Start();
+
+            //Assert
+            act.Should()
+               .Throw<InvalidOperationException>()
+               .WithMessage("*must specify* storage* UseStorage*");
+        }
+
+        [Fact]
+        public void Start_WhenActivatorIsNull_ShouldFailFast()
+        {
+            //Arrange
+            var agent = new Agent();
+            agent.UseStorage(_container.Get<IJobStorage>());
+
+            //Act
+            Action act = () => agent.Start();
+
+            //Assert
+            act.Should()
+               .Throw<InvalidOperationException>()
+               .WithMessage("*must specify* activator* UseActivator*");
+        }
+
+        [Fact]
+        public void Start_ShouldStartTheAgent()
+        {
+            //Arrange
+            var agent = new Agent();
+            var storage = _container.Get<IJobStorage>();
+            agent.UseStorage(storage)
+                 .UseActivator(_container.Get<IActivator>());
+            storage.PollForNextJobAsync().Returns(Task.FromResult<IJob>(null));
+
+            using (agent)
+            {
+                //Act
+                agent.Start();
+
+                //Assert
+                agent.IsRunning.Should().BeTrue();
+
+                //Act
+                agent.Stop(true);
+
+                //Assert
+                agent.IsRunning.Should().BeFalse();
+            }
+        }
+
+        [Fact]
+        public void UseConcurrentWorkers_WhenAgentIsStarted_ShouldFailFast()
+        {
+            //Arrange
+            var agent = new Agent();
+            var storage = _container.Get<IJobStorage>();
+            agent.UseStorage(storage)
+                 .UseActivator(_container.Get<IActivator>());
+            storage.PollForNextJobAsync().Returns(Task.FromResult<IJob>(null));
+
+            using (agent)
+            {
+                //Act
+                agent.Start();
+                Action act = () => agent.UseConcurrentWorkers(5);
+
+                //Assert
+                act.Should()
+                   .Throw<NotSupportedException>()
+                   .WithMessage("*number of concurrent workers cannot be changed* agent is running*");
+            }
+        }
+
+        [Fact]
+        public void Start_WhenAgentIsAlreadyStarted_ShouldFailFast()
+        {
+            //Arrange
+            var agent = new Agent();
+            var storage = _container.Get<IJobStorage>();
+            agent.UseStorage(storage)
+                 .UseActivator(_container.Get<IActivator>());
+            storage.PollForNextJobAsync().Returns(Task.FromResult<IJob>(null));
+
+            using (agent)
+            {
+                //Act
+                agent.Start();
+                Action act = () => agent.Start();
+
+                //Assert
+                act.Should()
+                   .Throw<InvalidOperationException>()
+                   .WithMessage("*agent* already running*");
+            }
+        }
+
+        [Fact]
+        public void Stop_WhenAgentIsDisposed_ShouldFailFast()
+        {
+            //Arrange
+            var agent = new Agent();
+            agent.Dispose();
+
+            //Act
+            Action act = () => agent.Stop(true);
+
+            //Assert
+            act.Should()
+               .Throw<ObjectDisposedException>()
+               .Which.ObjectName.Should().Be(nameof(Agent));
+        }
+
+        [Fact]
+        public void Dispose_WhenAgentIsDisposed_ShouldDoNothing()
+        {
+            //Arrange
+            var agent = new Agent();
+            agent.Dispose();
+
+            //Act
+            Action act = () => agent.Dispose();
+
+            //Assert
+            act.Should().NotThrow();
+        }
+    }
+}
